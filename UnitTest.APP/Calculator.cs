b1b2905c866f2625a23dba1f310af5fcd15b9e55
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-05-30 16:49:36 +0300

    xUnit Test, run test

---
+++ UnitTest.APP/Calculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest.APP
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+
+        }
+
+    }
+}
---
+++ UnitTest.Test/CalculatorTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTest.APP;
+using Xunit;
+
+namespace UnitTest.Test
+{
+    public  class CalculatorTest
+    {
+        [Fact]
+        public void AddTest()
+        {
+            // Arrange
+            // Biz burada değişkenleri initilaize ettiğimiz yerdir. Nesne örneğini de burada oluşturmaktadyız.
+            int a = 5;
+            int b = 5;
+            var calculator = new Calculator();
+
+
+
+            //Act
+            var total = calculator.Add(a, b);
+
+
+            //Assert
+            // Doğrulama Evresidir. Act den çıkan testin doğruluğunu sorgulamaktadır.
+            Assert.Equal<int>(10, total);
+        }
+    }
+}
