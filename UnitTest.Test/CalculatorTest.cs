62e8a065e7753242d9ce5be1c777c575c19e0d2c
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-05-31 13:50:04 +0300

    Contain/DoesNotContain

--- UnitTest.Test/CalculatorTest.cs
+++ UnitTest.Test/CalculatorTest.cs
@@ -13,21 +13,38 @@ namespace UnitTest.Test
         [Fact]
         public void AddTest()
         {
-            // Arrange
-            // Biz burada değişkenleri initilaize ettiğimiz yerdir. Nesne örneğini de burada oluşturmaktadyız.
-            int a = 5;
-            int b = 5;
-            var calculator = new Calculator();
+            //// Arrange
+            //// Biz burada değişkenleri initilaize ettiğimiz yerdir. Nesne örneğini de burada oluşturmaktadyız.
+            //int a = 5;
+            //int b = 5;
+            //var calculator = new Calculator();
+
+            ////Act
+            //var total = calculator.Add(a, b);
+
+            ////Assert
+            //// Doğrulama Evresidir. Act den çıkan testin doğruluğunu sorgulamaktadır.
+            //Assert.Equal<int>(10, total);
 
 
 
-            //Act
-            var total = calculator.Add(a, b);
+
+            // ilk parametre beklenne , ikincisi ise gerçek değeri karşılamaktadır.
+            //Assert.Contains("ahmet", " Berkan Çelik");
+
+            var names = new List<string>() { "Berkan", "Ahmet", "Mehmet" };
+
+            Assert.Contains(names, x => x == "Berkan");
+
+            
 
 
-            //Assert
-            // Doğrulama Evresidir. Act den çıkan testin doğruluğunu sorgulamaktadır.
-            Assert.Equal<int>(10, total);
+
+
+
+
+
+
         }
     }
 }
