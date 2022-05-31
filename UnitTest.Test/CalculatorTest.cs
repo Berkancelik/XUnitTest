0b77dfbb2561abbb083d2b6dc50d633ad56ed0c3
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-05-31 13:56:10 +0300

    True or False

--- UnitTest.Test/CalculatorTest.cs
+++ UnitTest.Test/CalculatorTest.cs
@@ -13,37 +13,13 @@ namespace UnitTest.Test
         [Fact]
         public void AddTest()
         {
-            //// Arrange
-            //// Biz burada değişkenleri initilaize ettiğimiz yerdir. Nesne örneğini de burada oluşturmaktadyız.
-            //int a = 5;
-            //int b = 5;
-            //var calculator = new Calculator();
-
-            ////Act
-            //var total = calculator.Add(a, b);
-
-            ////Assert
-            //// Doğrulama Evresidir. Act den çıkan testin doğruluğunu sorgulamaktadır.
-            //Assert.Equal<int>(10, total);
+            Assert.True(5 > 20);
+            Assert.True(5 > 2);
+            Assert.False(5 > 20);
 
 
 
-
-            // ilk parametre beklenne , ikincisi ise gerçek değeri karşılamaktadır.
-            //Assert.Contains("ahmet", " Berkan Çelik");
-
-            var names = new List<string>() { "Berkan", "Ahmet", "Mehmet" };
-
-            Assert.Contains(names, x => x == "Berkan");
-
-            
-
-
-
-
-
-
-
+            Assert.True("".GetType()==typeof(string));
 
         }
     }
